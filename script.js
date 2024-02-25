a48632d07f66e003caba0256e8fccefea7c552ae
NinaLeth <126472639+NinaLeth@users.noreply.github.com>
2024-02-25 21:30:04 +0100

    Sidste kode

--- script.js
+++ script.js
@@ -68,10 +68,27 @@ function gemMenu() {
 
 kryds.onclick= gemMenu;
 
+/*pil bag burgermenu
+
+function flytPilUnderMenu() {
+    const pil = document.getElementById('pil');
+    const burgermenu = document.getElementById('nav_menu');
+  
+    if (burgermenu.style.display === 'none') {
+        // Vis burgermenuen
+        burgermenu.style.display = 'block';
+        pil.style.display = 'none'; //pilen fjernes
+      } else {
+        // Skjul burgermenuen
+        burgermenu.style.display = 'none';
+        pil.style.display = 'block' // pilen vises
+      }
+    }*/
+
 /*KNAPPER*/
 
 document.addEventListener("DOMContentLoaded", function() {
-    const links = document.querySelectorAll('.wrapper div');
+    const links = document.querySelectorAll('.wrapper div, .grid43, .grid44');
 
     links.forEach(link => {
         link.addEventListener('click', function() {
@@ -80,6 +97,7 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     });
 });
+
 
 /*LYD*/
 
@@ -97,3 +115,80 @@ knap_gellerup.onclick = function () { //at den pauser og starter igen når man k
         lyd.pause();
     }
 };
+
+const knap_gelleruphojskole = document.getElementById("grid14");
+const lyd1 = document.getElementById("lyd_gellerup1");
+
+knap_gelleruphojskole.onclick = function(){ //når man klikker på knappen går lyden igang
+    lyd1.play();
+};
+
+knap_gelleruphojskole.onclick = function () { //at den pauser og starter igen når man klikker
+    if (lyd1.paused) {
+        lyd1.play();
+    } else {
+        lyd1.pause();
+    }
+};
+
+const knap_bk = document.getElementById("grid25");
+const lyd2 = document.getElementById("lyd_bk1");
+
+knap_bk.onclick = function(){ //når man klikker på knappen går lyden igang
+    lyd2.play();
+};
+
+knap_bk.onclick = function () { //at den pauser og starter igen når man klikker
+    if (lyd2.paused) {
+        lyd2.play();
+    } else {
+        lyd2.pause();
+    }
+};
+
+const knap_gelleruphojskole2 = document.getElementById("grid111");
+const lyd3 = document.getElementById("lyd_gellerup2");
+
+knap_gelleruphojskole2.onclick = function(){ //når man klikker på knappen går lyden igang
+    lyd3.play();
+};
+
+knap_gelleruphojskole2.onclick = function () { //at den pauser og starter igen når man klikker
+    if (lyd3.paused) {
+        lyd3.play();
+    } else {
+        lyd3.pause();
+    }
+};
+
+const knap_bke = document.getElementById("grid34");
+const lyd4 = document.getElementById("lyd_bk");
+
+knap_bke.onclick = function(){ //når man klikker på knappen går lyden igang
+    lyd4.play();
+};
+
+knap_bke.onclick = function () { //at den pauser og starter igen når man klikker
+    if (lyd4.paused) {
+        lyd4.play();
+    } else {
+        lyd4.pause();
+    }
+};
+
+const knap_grim = document.getElementById("grid38");
+const lyd5 = document.getElementById("lyd_grim");
+
+knap_grim.onclick = function(){ //når man klikker på knappen går lyden igang
+    lyd5.play();
+};
+
+knap_grim.onclick = function () { //at den pauser og starter igen når man klikker
+    if (lyd5.paused) {
+        lyd5.play();
+    } else {
+        lyd5.pause();
+    }
+};
+
+
